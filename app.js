f830136883d80465a35bc4d8a9a9ce5a4d50c00a
Giorgi Ratiani <gembani0@gmail.com>
2023-02-17 18:54:26 +0400

    javascript work

---
+++ app.js
@@ -0,0 +1,18 @@
+// 1. Დაწერეთ ფუნქცია რომელსაც გადაეცემა integer ების array მაგალითად
+// [10,20,-3,-4,6,45,67] და დააბრუნებს ორი ყველაზე პატარა დადებითი რიცხვის ჯამს ამ
+// შემთხვევაში იქნება 16 ~ (არ გამოიყენოთ ჩაშენებული ფუნქციები მხოლოდ შეგიძლიათ
+// გამოიყენოდ splice)
+
+
+
+function getIntiger(intiger){
+    let minNum = [0]
+    for(let number in intiger){
+        if(minNum > intiger[number]){
+            
+        }
+    }
+    console.log(minNum)
+}
+
+getIntiger([10,20,-3,-4,6,45,67])
